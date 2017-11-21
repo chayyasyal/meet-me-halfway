a759f72857dc725652bb39248c33e525b0ed0315
oliverpople <oliverpople@gmail.com>
2017-11-21 13:42:53 +0000

    Set up environment with node, express and handlebars

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express();
+
+app.disable('x-powered-by'); // improves security by hiding info from the header
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'}); // set default layout to main.handlebars
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static('meet-me-halfway' + '/public')); //lets you reference public folder/imgaes
+
+app.get('/', function(req, res){
+  res.render('home');  //renders home.handlebars
+});
+
+app.listen(app.get('port'), function() {   // defines what port we listne to
+  console.log('Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate');
+});
