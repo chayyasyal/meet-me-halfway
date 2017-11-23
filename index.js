27a41f279bff9853ea6db9408bf5985cd67f834a
oliverpople <oliverpople@gmail.com>
2017-11-23 10:25:09 +0000

    Refactored places js

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
 function initializeProject() {
 
-  fetch("http://ip-api.com/json").then(res => res.json()).then(function (usersLocation) {
+  fetch("http://ip-api.com/json").then(res => res.json()).then(function(usersLocation) {
     var mapper = new GoogleMapGenerator(usersLocation);
 
     var map = mapper.initMap();
 
 
-    document.getElementById('submit').addEventListener('click', function () {
+    document.getElementById('submit').addEventListener('click', function() {
       var geocoder = new GeoCoder(map);
       var address1 = document.getElementById('address1').value;
       var address2 = document.getElementById('address2').value;
       var address1Points = geocoder.setMarker(address1);
-      var ddress2Points = geocoder.setMarker(address2);
+      var address2Points = geocoder.setMarker(address2);
 
-      geocoder.calculateMiddlePoint(address1, address2).then(function (midpoint) {
-        console.log(midpoint, "in eventlistener")
-        var potPlaces = new PlaceSearcher(map, midpoint).returnPlaces();
-        return potPlaces;
+      geocoder.calculateMiddlePoint(address1, address2)
+        .then(function(midpoint) {
+          console.log(midpoint, "in eventlistener")
+          PlaceSearcher(map, midpoint).then(function(results) {
+              console.log("in promise2222", results);
+              console.log("in promise length", results.length)
 
-      }).then(function(potPlaces){
-        console.log("a list of potential places");
-          console.log(potPlaces);
-          console.log(potPlaces.length, "length")
-          for (var i=0; i<potPlaces.length; i++){
-            console.log(potPlaces[i].geometry.location.lat());
-          }
-      })
+            })
+
+            .catch(function(status) {
+              alert(status);
+            });
+        }).catch(function(status) {
+          alert(status);
+        });
 
     });
-
-  }).catch(err => {
-    throw err
   });
-
 }
+//       then(function(potPlaces){
+//         console.log("a list of potential places");
+//           console.log(potPlaces);
+//           console.log(potPlaces.length, "length")
+//           for (var i=0; i<potPlaces.length; i++){
+//             console.log(potPlaces[i].geometry.location.lat());
+//           }
+//       })
+//
+//     });
+//
+//   }).catch(err => {
+//     throw err
+//   });
+//
+// }
--- places.js
+++ places.js
@@ -1,36 +1,25 @@
+(function(exports) {
+  exports.PlaceSearcher = function(map, midpoint, types = 'cafe') {
+    console.log(midpoint[0] + ',' + midpoint[1] + "  in placesearcher ");
+    var myLatlng = new google.maps.LatLng(parseFloat(midpoint[0]), parseFloat(midpoint[1]));
 
-(function (exports) {
-  exports.PlaceSearcher = function(map, midpoint, type = 'cafe'){
-    console.log(midpoint[0]+','+midpoint[1]+"  in placesearcher ");
-    var myLatlng = new google.maps.LatLng(parseFloat(midpoint[0]),parseFloat(midpoint[1]));
-    
     var _request = {
-        location: myLatlng,
-        radius: '500',
-        type: ['cafe']
-      };
+      location: myLatlng,
+      radius: '500',
+      type: types
+    };
 
-      var _places = [];
-
-      
-      _callback = function(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            var place = results[i];
-            _places.push(results[i]);
-          }
-        }
-      }
-
+    return new Promise(function(resolve, reject) {
       var service = new google.maps.places.PlacesService(map);
-      service.nearbySearch(_request, _callback);
-      
-      returnPlaces = function(){
-        return _places;
-      }
-
-
-      return {returnPlaces:returnPlaces}
-    }
-
+      service.nearbySearch(_request, function(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          // resolve promise with results on OK status
+          resolve(results);
+        } else {
+          // reject promise otherwise
+          reject(status);
+        }
+      });
+    });
+  }
 })(this);
